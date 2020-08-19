bb35973110b6b59827ea6ca7f4032856fe2f5a26
Thomas L Durbin <tdurbin@thomasldurbin.com>
2020-08-18 19:12:16 -0500

    Fixed Formatting in Config

--- app/Config/Pager.php
+++ app/Config/Pager.php
@@ -18,10 +18,10 @@ class Pager extends BaseConfig
 	|
 	*/
 	public $templates = [
-		'default_full'   => 'CodeIgniter\Pager\Views\default_full',
-		'default_regular'=> 'CodeIgniter\Pager\Views\default_regular',
-		'default_simple' => 'CodeIgniter\Pager\Views\default_simple',
-		'default_head'   => 'CodeIgniter\Pager\Views\default_head',
+		'default_full'    => 'CodeIgniter\Pager\Views\default_full',
+		'default_regular' => 'CodeIgniter\Pager\Views\default_regular',
+		'default_simple'  => 'CodeIgniter\Pager\Views\default_simple',
+		'default_head'    => 'CodeIgniter\Pager\Views\default_head',
 	];
 
 	/*
